16fd701c786a7f697a0b148245f5449c54eb1107
Mahbub Rahim <mahbubrahim926@gmail.com>
2024-06-03 15:53:47 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
-export const BASE_URL= "http://localhost:3006/";
-// export const BASE_URL= "https://mea-backend.vercel.app/";
+// export const BASE_URL= "http://localhost:3006/";
+export const BASE_URL= "https://mea-backend.vercel.app/";
 export const S3_BASE_URL= "https://practicemania.s3.ap-south-1.amazonaws.com";
 export const API_LEVEL="/api/v1";
 export const APP_URL= "http://localhost:3000/";
